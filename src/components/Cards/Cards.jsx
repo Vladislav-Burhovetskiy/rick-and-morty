f5de092cd831a955cc904379f7ac0d2bb8e9574c
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-03-16 20:28:32 +0200

    To make adaptive (tablet / desktop)

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../../styles/components/Cards.scss'
+import './Cards.scss'
 
 const Cards = ({ results, search }) => {
   let displayResult;
@@ -10,26 +10,26 @@ const Cards = ({ results, search }) => {
     displayResult = filteredResults.length ? filteredResults.map(person => {
       const { id, name, image, species } = person;
       return (
-        <div className='cards'>
-          <div key={id} className='cards__characters'>
-            <div className='cards__characters__image_container'>
-              <img 
-                src={image} 
-                alt={name} 
-                className='cards__characters__image'
-              />
-            </div>
-            <div className='cards__characters__info'>
-              <h3 className='cards__characters__info_name'>{name}</h3>
-              <p className='cards__characters__info_species'>{species}</p>
-            </div>
+        
+        <div key={id} className='cards__characters'>
+          <div className='cards__characters__image_container'>
+            <img 
+              src={image} 
+              alt={name} 
+              className='cards__characters__image'
+            />  
+          </div>
+          <div className='cards__characters__info'>
+            <h3 className='cards__characters__info_name'>{name}</h3>
+            <p className='cards__characters__info_species'>{species}</p>
           </div>
         </div>
+        
       )
     }) : <div className='cards__characters_notfind'>No characters with this name :/</div>
   } 
 
-  return <>{displayResult}</>
+  return <div className='cards'>{displayResult}</div>
 }
 
 export default Cards
